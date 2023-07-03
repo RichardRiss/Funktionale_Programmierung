8fbedee7e742d2f088a36b0198795b7dd60ab984
Richard Riß <richard.riss@desy.de>
2023-07-03 13:25:52 +0200

    ...

--- Uebung06/6.2.hs
+++ Uebung06/6.2.hs
@@ -1,16 +1,71 @@
-import Control.Monad (MonadPlus (..))
+import Control.Monad (MonadPlus (..), guard, msum) 
+
 
 {-
-    
+
+Definiert eine Funktion permutations :: MonadPlus mp => [a] -> mp [a], die alle Permutationen einer übergebenen Liste berechnet. 
+Wenn ihr die Nichtdeterminismussicht hierbei verwenden wollt, kann es sinnvoll sein, zunächst eine Hilfsfunktion zu definieren, 
+welche ein Element nichtdeterministisch an jede beliebe Position einer Liste einfügt.
+
+permutations :: MonadPlus mp => [a] -> mp [a]
+permutations = undefined
+
+insert :: MonadPlus mp => e -> [e] -> mp [e]
+insert = undefined
+Stell euch für mp, also vor, dass es hier Nichtdeterminismus gibt und ihr mplus nutzt um zwei mögliche Lösungen zu kombinieren.
+
+Überlegt euch fürs Testen für welche Datentypen eine Instanzen für MonadPlus definiert ist.
+        
 -}
-
-import Control.Monad (MonadPlus (..), guard)
-
-insert :: MonadPlus m => a -> [a] -> m [a]
-insert x xs = msum $ [ return $ front ++ [x] ++ back | i <- [0..length xs], let (front, back) = splitAt i xs ]
 
 permutations :: MonadPlus m => [a] -> m [a]
 permutations [] = return []
 permutations (x:xs) = do
     ys <- permutations xs
     insert x ys
+
+
+insert :: MonadPlus m => e -> [e] -> m [e]
+insert x xs = monadConcat [ return(front ++ [x] ++ back) | i <- [0..length xs], let front = take i xs, let back = drop i xs ]
+
+monadConcat :: MonadPlus m => [m a] -> m a
+monadConcat [] = mzero
+monadConcat (x:xs) = x `mplus` monadConcat xs
+--monadConcat = foldr mplus mzero
+-- Alternativ mit msum aus Control.Monad
+
+
+
+
+{-
+Definiert weiter eine Funktion solve, so dass solve l s alle Listen der Länge l von natürlichen Zahlen berechnet, die sich zu s summieren lassen:
+
+-}
+
+solve :: MonadPlus mp => Int -> Int -> mp [Int]
+solve 0 0 = return []
+solve l s = do
+  first <- monadConcat $ map return [0..s]
+  res <- solve (l - 1) (s - first)
+  return $ first : res
+
+
+
+
+
+-- Test
+main :: IO ()
+main = do
+    print(permutations [1,2,3] :: [[Int]])
+
+
+
+
+
+
+
+
+    
+    
+    
+ 
